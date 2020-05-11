58d59722139675d2d423cd52dafa25ee186e93b0
venividivicit <mbobojonov@hotmail.com>
2020-05-10 23:27:40 -0400

    test for best combination

--- render.py
+++ render.py
@@ -1,12 +1,16 @@
 # make convenutinal neaural network
 import tensorflow as tf
-from tensorflow.keras.datasets import cifar10
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import TensorBoard
 import numpy as np
 import pickle
+import time
+
+
+
+tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
 
 IMG_SIZE = 100;
 
@@ -18,24 +22,46 @@ y = np.array(y)
 #normalize the data, scale it
 X = X/255
 
-model = Sequential()
-model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:]))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
+#test for best combination
+dense_layers = [0, 1, 2]
+layer_sizes = [32, 64, 128]
+conv_layers = [1, 2, 3]
 
-model.add(Conv2D(64, (3,3)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+            NAME = "{}-conv-{}-nodes-{}-dense-{}".format(conv_layer, layer_size, dense_layer, int(time.time()))
+            print(NAME)
 
-# dense needs 1D dataset
-model.add(Flatten())
+            model = Sequential()
 
-model.add(Dense(64))
-model.add(Activation("relu"))
+            model.add(Conv2D(layer_size, (3, 3), input_shape=X.shape[1:]))
+            model.add(Activation('relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Dense(1))
-model.add(Activation("sigmoid"))
+            for l in range(conv_layer-1):
+                model.add(Conv2D(layer_size, (3, 3)))
+                model.add(Activation('relu'))
+                model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+            model.add(Flatten())
 
-model.fit(X, y, batch_size=32, epochs=5, validation_split=0.3)
+            for _ in range(dense_layer):
+                model.add(Dense(layer_size))
+                model.add(Activation('relu'))
+
+            model.add(Dense(1))
+            model.add(Activation('sigmoid'))
+
+            tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+            model.compile(loss='binary_crossentropy',
+                          optimizer='adam',
+                          metrics=['accuracy'],
+                          )
+
+            model.fit(X, y,
+                      batch_size=32,
+                      epochs=10,
+                      validation_split=0.3,
+                      callbacks=[tensorboard])
